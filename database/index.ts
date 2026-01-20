0b8efb377e401376792ffcb3c032a792fcf65fe9
Thimeth Sathmika <sathmikakb@gmail.com>
2026-01-20 07:02:08 +0530

    index file that defines the db models for ease access

---
+++ database/index.ts
@@ -0,0 +1,9 @@
+// Export all models from a single entry point
+export { default as User } from './user.model';
+export { default as Seetu } from './seetu.model';
+export { default as Placement } from './placement.model';
+
+// Export types
+export type { IUser } from './user.model';
+export type { ISeetu } from './seetu.model';
+export type { IPlacement } from './placement.model';
